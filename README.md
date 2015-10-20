b55efaf3fbc5eb984c862652e9be35f3a80f5e95
liang <603590822@qq.com>
2015-10-20 16:46:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UITestDemo
+This is a Demo for UITest.
+For more information,please go to http://blog.csdn.net/lyqfly07/article/details/49252143
---
+++ UITestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITestDemo
+//
+//  Created by liangyiqi on 15/10/20.
+//  Copyright © 2015年 LYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
